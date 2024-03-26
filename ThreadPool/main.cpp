9ed3a1f9ededd4e4d492656d800bfaa9b53303e4
Sushwee <107102178+Sushwee@users.noreply.github.com>
2024-03-26 10:04:18 +0800

    Add files via upload

---
+++ ThreadPool/main.cpp
@@ -0,0 +1,72 @@
+﻿// 线程池项目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+#include "threadpool.h"
+
+using uLong = unsigned long long;
+
+class MyTask : public Task
+{
+public:
+    MyTask(int begin, int end)
+        : begin_(begin)
+        , end_(end)
+    {}
+ 
+    Any run()  
+    {
+        std::cout << "tid:" << std::this_thread::get_id()
+            << "begin!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        uLong sum = 0;
+        for (uLong i = begin_; i <= end_; i++)
+            sum += i;
+        std::cout << "tid:" << std::this_thread::get_id()
+            << "end!" << std::endl;
+
+        return sum;
+    }
+
+private:
+    int begin_;
+    int end_;
+};
+
+int main()
+{
+   
+    {
+        ThreadPool pool;
+        // 用户自己设置线程池的工作模式
+        pool.setMode(PoolMode::MODE_CACHED);
+        // 开始启动线程池
+        pool.start(4);
+
+        // 如何设计这里的Result机制呢
+        Result res1 = pool.submitTask(std::make_shared<MyTask>(1, 100000000));
+        Result res2 = pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        Result res3 = pool.submitTask(std::make_shared<MyTask>(200000001, 300000000));
+        pool.submitTask(std::make_shared<MyTask>(200000001, 300000000));
+
+        pool.submitTask(std::make_shared<MyTask>(200000001, 300000000));
+        pool.submitTask(std::make_shared<MyTask>(200000001, 300000000));
+
+        // 随着task被执行完，task对象没了，依赖于task对象的Result对象也没了
+        uLong sum1 = res1.get().cast_<uLong>();  // get返回了一个Any类型，怎么转成具体的类型呢？
+        uLong sum2 = res2.get().cast_<uLong>();
+        uLong sum3 = res3.get().cast_<uLong>();
+
+        // Master - Slave线程模型
+        // Master线程用来分解任务，然后给各个Slave线程分配任务
+        // 等待各个Slave线程执行完任务，返回结果
+        // Master线程合并各个任务结果，输出
+        cout << (sum1 + sum2 + sum3) << endl;
+    }
+
+    getchar();
+}
