843820bc7b1d7dd9d053fd5abd3f6757f51712ef
Sushwee <107102178+Sushwee@users.noreply.github.com>
2024-03-26 10:04:41 +0800

    Add files via upload

---
+++ 使用可变参模板/main.cpp
@@ -0,0 +1,57 @@
+﻿// 线程池项目-最终版.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <future>
+#include <chrono>
+using namespace std;
+
+#include "threadpool.h"
+
+int count(int a, int b)
+{
+    this_thread::sleep_for(chrono::seconds(2));
+    return a + b;
+}
+int mul(int a, int b, int c)
+{
+    this_thread::sleep_for(chrono::seconds(2));
+    return a * b * c;
+}
+
+int main()
+{
+    ThreadPool pool;
+    // pool.setMode(PoolMode::MODE_CACHED);
+    pool.start(2);
+
+    future<int> r1 = pool.submitTask(count, 1, 2);
+    future<int> r2 = pool.submitTask(mul, 1, 2, 3);
+    future<int> r3 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    future<int> r4 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    future<int> r5 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    //future<int> r4 = pool.submitTask(sum1, 1, 2);
+
+    cout << r1.get() << endl;
+    cout << r2.get() << endl;
+    cout << r3.get() << endl;
+    cout << r4.get() << endl;
+    cout << r5.get() << endl;
+}
